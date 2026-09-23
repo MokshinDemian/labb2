85e0082acb4261b9cdc558f620d7a6bc5bc03c53
MokshinDemian <89525512494demon@gmail.com>
2026-09-23 22:43:54 +0300

    dz2.c

--- dz2.c
+++ dz2.c
@@ -4,7 +4,7 @@ int main() {
          setlocale(LC_ALL, ".UTF8");
          int glov = 300; // стоимость перчаток
          int ryk = 1000; // стоимость рюкзака
-         int c = 100; // стоимость галстука
+         int gals = 100; // стоимость галстука
          int d = 2000; // общая
          printf("Сдача: %d", d - (ryk + gals + glov));
          return 0;
