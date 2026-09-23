71eb8a6bba216061263318fee624cf51f33a265c
MokshinDemian <89525512494demon@gmail.com>
2026-09-23 18:49:01 +0300

    Update dz2.c

--- dz2.c
+++ dz2.c
@@ -5,9 +5,8 @@ int main() {
 	int a = 300;
 	int b = 1000;
 	int c = 100;
-	int d;
+	int d = 2000;
 	printf("Введите исходную сумму ");
-	scanf_s("%d", &d);
 	printf("Сдача: %d", d - (a + b + c));
 	return 0;
 }
